be2c638bbdec25cfb28bbc7fd79cb35dac87f9ba
nayla <nayla.arviandini@gmail.com>
2026-03-13 10:25:04 +0700

    delete fragment__register__catat_lari dari main

--- app/src/main/java/com/uchiha/catatlari/MainActivity.kt
+++ app/src/main/java/com/uchiha/catatlari/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.uchiha.catatlari
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.uchiha.catatlari.R
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_catatlari)
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, WelcomeFragment())
-            .commit()
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
--- app/src/main/java/com/uchiha/catatlari/fragment_welcome.kt
+++ app/src/main/java/com/uchiha/catatlari/fragment_welcome.kt
@@ -5,13 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.uchiha.catatlari.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentWelcomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_beranda, container, false)
+        binding= FragmentWelcomeBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnMulai.setOnClickListener{
+            findNavController().navigate(
+                WelcomeFragmentDirections.actionWelcomeFragmentToRegisterCatatLari2()
+            )
+        }
     }
 }
